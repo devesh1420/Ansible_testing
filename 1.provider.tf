62f783948e99d719f9a84cd6ef4d8698fb50d7b3
devesh1420 <devesh1452000@gmail.com>
2025-01-28 15:14:33 +0530

    up

--- 1.provider.tf
+++ 1.provider.tf
@@ -11,10 +11,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "tfworkspacesbucket"
-    key            = "Ansible.tfstate"
-    region         = "us-east-1"
+    bucket = "tfworkspacesbucket"
+    key    = "Ansible.tfstate"
+    region = "us-east-1"
     # dynamodb_table = "-terraform-locks"
-    encrypt        = true
+    encrypt = true
   }
 }
